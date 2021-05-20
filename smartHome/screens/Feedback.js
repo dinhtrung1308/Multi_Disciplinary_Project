03ee7583352049a53284d90fe7e5f248f3bb0dba
Trung <trungduong13082000@gmail.com>
2021-05-20 18:29:05 +0700

    Add Feedback screen

--- smartHome/screens/Feedback.js
+++ smartHome/screens/Feedback.js
@@ -1,24 +1,79 @@
+
 import React from 'react';
 import {
+    StyleSheet,
+    SafeAreaView,
     View,
     Text,
-} from "react-native";
+    Image,
+    TouchableOpacity
+} from 'react-native';
 import { Header, Left, Right, Icon, Body, Content } from 'native-base';
-function Feedback({navigation}){
+import {images, COLORS, FONTS, SIZES} from './constants';
+
+
+
+const Feedback = ({ navigation }) => {
+
+    // Render
+
     return (
-        <View style={ {flex: 1} }>
+        <SafeAreaView style={styles.container}>
+<View style={ {flex: 1} }>
              <Header style={ {backgroundColor:'black'} }>
                 <Left>
                     <Icon name="menu" style={{color: '#b06f13'}} onPress={ () => navigation.openDrawer() } />
                 </Left>
-                <Body />
-                <Right />
+  
             </Header>
-            <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-                <Text> Feedback </Text>
-            </View>
+            
         </View>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                    source={images.feedbackRobot}
+                    resizeMode="contain"
+                    style={{
+                        width: '160%',
+                        height: '160%',
+                    }}
+                />
+            </View>
+
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={{ alignItems: 'center', marginHorizontal: SIZES.padding }}>
+                    <Text style={{ ...FONTS.h2 }}>Feedback</Text>
+                    <Text style={{ color: COLORS.gray, marginTop: SIZES.padding, textAlign: 'center', ...FONTS.body3 }}>When you have a problem, please tell us! </Text>
+                </View>
+
+                <TouchableOpacity
+                    style={[styles.shadow, { marginTop: SIZES.padding * 2, width: '70%', height: 50, alignItems: 'center', justifyContent: 'center' }]}
+                    onPress={() => navigation.navigate("Home")}
+                >
+                    
+                        <Text style={{ color: COLORS.yellowBrown, ...FONTS.h3 }}>Send!</Text>
+                    
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    }
+});
 
 export default Feedback;
