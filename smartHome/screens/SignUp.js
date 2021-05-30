fe66e32e19369f3668485d2287b6a3173b47f9c7
KaNology <61189836+KaNology@users.noreply.github.com>
2021-05-30 10:54:47 +0700

    Sign Up implementation
    Upload User data to database

--- smartHome/screens/SignUp.js
+++ smartHome/screens/SignUp.js
@@ -1,15 +1,177 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
-    Text
+    Text,
+    StyleSheet,
+    ImageBackground,
+    TouchableOpacity,
+    TextInput,
+    Dimensions
 } from "react-native";
 
-function SignUp({navigation}){
+import { images } from '../constants';
+import appTheme from '../constants/theme';
+// npm install axios
+const windoWidth = Dimensions.get('window').width;
+
+function SignUp({ navigation }) {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = () => {
+
+        // axios.post('http://localhost:4000/app/signup', registered)
+        // .then(response => console.log(response.data))
+
+        fetch('http://10.0.2.2:3000/send-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                fullName: fullName,
+                username: username,
+                email: email,
+                password: password,
+            })
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data)
+                navigation.navigate('Login')
+            }).catch(err => {
+                console.log("error", err)
+            })
+
+        setFullName('')
+        setEmail('')
+        setUsername('')
+        setPassword('')
+    };
+
     return (
-        <View>
-            <Text>SignUp</Text>
-        </View>
+        <ImageBackground
+            style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: 'black',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+            imageStyle={{ opacity: 0.5 }}
+            source={images.backgroundImg}>
+            <View style={styles.background}>
+                <View>
+                    <Text style={styles.titleStyle}>Sign Up</Text>
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Full Name'}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid='transparent'
+                        onChangeText={fullName => setFullName(fullName)}
+                        defaultValue={fullName}
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Email'}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid='transparent'
+                        onChangeText={email => setEmail(email)}
+                        defaultValue={email}
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'username'}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid='transparent'
+                        onChangeText={username => setUsername(username)}
+                        defaultValue={username}
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Password'}
+                        secureTextEntry={true}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid='transparent'
+                        onChangeText={password => setPassword(username)}
+                        defaultValue={password}
+                    />
+                </View>
+
+                <TouchableOpacity
+                    onPress={() =>
+                        onSubmit()
+                    }
+                    style={styles.btnSignUp}
+                >
+                    <Text style={styles.text}>Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    background: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        width: "90%",
+        paddingBottom: 10,
+        borderRadius: 20
+    },
+    titleStyle: {
+        color: 'gold',
+        fontSize: 40,
+        fontWeight: 'bold',
+        fontFamily: appTheme.FONTS.largeTitle.fontFamily
+    },
+    input: {
+        width: windoWidth - 55,
+        height: 50,
+        borderRadius: 45,
+        fontSize: 16,
+        paddingLeft: 45,
+        backgroundColor: 'rgba(0,0,0,0.35)',
+        color: 'rgba(255,255,255,0.7)',
+        marginHorizontal: 25,
+    },
+    inputContainer: {
+        marginBottom: 10,
+    },
+    text: {
+        color: 'rgba(255,255,255,1)',
+        fontSize: 16,
+        textAlign: 'center'
+    },
+    btnLogin: {
+        width: windoWidth - 55,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: 'gold',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    btnSignUp: {
+        width: windoWidth - 55,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: 'green',
+        justifyContent: 'center'
+    }
+});
 
 export default SignUp;
